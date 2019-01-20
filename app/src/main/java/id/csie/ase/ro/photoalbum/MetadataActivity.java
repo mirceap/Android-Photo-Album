3432da9599cf2e3d74dd659df1665f25f0d3789d
mirceap <pavelmircea92@gmail.com>
2019-01-20 20:19:56 +0200

    After cleanup.

--- app/src/main/java/id/csie/ase/ro/photoalbum/MetadataActivity.java
+++ app/src/main/java/id/csie/ase/ro/photoalbum/MetadataActivity.java
@@ -2,11 +2,7 @@ package id.csie.ase.ro.photoalbum;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.media.ExifInterface;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -17,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -72,19 +67,11 @@ public class MetadataActivity extends AppCompatActivity {
         if (bundle != null){
             image.setImageResource(bundle.getInt("Image"));
 
-            String name = getResources().getResourceName(bundle.getInt("Image"));
-            Uri path = Uri.parse(name);
-//            String imgPath = path.toString();
             int resourceId = bundle.getInt("Image");
             Resources resources = this.getResources();
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inJustDecodeBounds = true;
-
-
-            Drawable drawable = getResources().getDrawable(resourceId);
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),resourceId);
-
 
             File externalFile = null;
             try {
@@ -102,51 +89,14 @@ public class MetadataActivity extends AppCompatActivity {
 
             width.setText(Integer.toString(imageWidth));
             height.setText(Integer.toString(imageHeight));
-
-////            Uri uri = new Uri.Builder()
-////                    .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-////                    .authority(resources.getResourcePackageName(resourceId))
-////                    .appendPath(resources.getResourceTypeName(resourceId))
-////                    .appendPath(resources.getResourceEntryName(resourceId))
-////                    .build();
-//
-//            ExifInterface exif = null;
-////            File file = new File(uri.toString().concat(".jpg"));
-////            FileInputStream fileInputStream = null;
-////            try {
-////                fileInputStream = new FileInputStream(file);
-////            } catch (FileNotFoundException e) {
-////                e.printStackTrace();
-////            }
-//            try {
-//                exif = new ExifInterface(externalFile.getAbsolutePath());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            //openFileInput(uri.toString().concat("jpg"))
-//            System.out.println(exif);
-
-//            BitmapDrawable bitmapDrawable = ((BitmapDrawable) image.getDrawable());
-//            Bitmap bitmap = bitmapDrawable .getBitmap();
-//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//            byte[] imageInByte = stream.toByteArray();
-//            ByteArrayInputStream bis = new ByteArrayInputStream(imageInByte);
         }
     }
 
 
 
     private File createExternalStoragePrivateFile(int resourceId, String filename) throws IOException {
-        // Create a path where we will place our private file on external
-        // storage.
         File file = new File(getExternalFilesDir(null), filename.concat(".jpg"));
 
-        // Very simple code to copy a picture from the application's
-        // resource into the external file.  Note that this code does
-        // no error checking, and assumes the picture is small (does not
-        // try to copy it in chunks).  Note that if external storage is
-        // not currently mounted this will silently fail.
         InputStream is = getResources().openRawResource(resourceId);
         OutputStream os = new FileOutputStream(file);
         byte[] data = new byte[is.available()];
@@ -155,13 +105,9 @@ public class MetadataActivity extends AppCompatActivity {
         is.close();
         os.close();
         return file;
-        // Unable to create file, likely because external storage is
-        // not currently mounted.
     }
 
     void deleteExternalStoragePrivateFile(String filename) {
-        // Get path for the file on external storage.  If external
-        // storage is not currently mounted this will fail.
         File file = new File(getExternalFilesDir(null), filename.concat(".jpg"));
         if (file != null) {
             file.delete();
@@ -169,8 +115,6 @@ public class MetadataActivity extends AppCompatActivity {
     }
 
     boolean hasExternalStoragePrivateFile(String filename) {
-        // Get path for the file on external storage.  If external
-        // storage is not currently mounted this will fail.
         File file = new File(getExternalFilesDir(null),  filename.concat(".jpg"));
         if (file != null) {
             return file.exists();
--- app/src/main/java/id/csie/ase/ro/photoalbum/RatingHelper.java
+++ app/src/main/java/id/csie/ase/ro/photoalbum/RatingHelper.java
@@ -1,7 +1,5 @@
 package id.csie.ase.ro.photoalbum;
 
-import android.content.Intent;
-
 public interface RatingHelper {
     void updateRating(float rating);
 }
